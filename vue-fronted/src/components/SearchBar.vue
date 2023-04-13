313975ad3d9eacd034ef22a1d6724696f8082f78
Loyage <792058350@qq.com>
2023-04-13 21:45:55 +0800

    ✨ feat(搜索框): 添加搜索框文件
    #8

---
+++ vue-fronted/src/components/SearchBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div style="display: flex">
+    <input type="text" class="search_input" placeholder="搜索关键词:" />
+    <button class="search_button">
+      <img src="../assets/acgpage/SearchLogo.png" />
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SearchBar",
+  data() {
+    return {};
+  },
+};
+</script>
+<style lang="scss" scoped></style>
