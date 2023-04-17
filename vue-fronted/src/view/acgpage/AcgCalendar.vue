d841779796eee9265cdf993686d4e560cb5ce0ea
wangyang <1264518726@qq.com>
2023-04-17 23:23:15 +0800

    update

--- vue-fronted/src/view/acgpage/AcgCalendar.vue
+++ vue-fronted/src/view/acgpage/AcgCalendar.vue
@@ -5,7 +5,7 @@
         <div class="Calendar_L">
           <img src="../../assets/acgpage/CalendarLogo.png" alt="logo" />
         </div>
-          <p>追番周历</p>
+        <p>追番周历</p>
       </div>
       <div class="Search_All">
         <p>查看全部 ></p>
@@ -24,19 +24,21 @@
       <img src="../../assets/acgpage/Line.png" alt="line" />
       <div class="content">
         <div class="col1">
+          <img src="https://s3-alpha-sig.figma.com/img/a16e/21de/05116c7e23b02b910bbbb0797e6a007c?Expires=1682899200&Signature=V3dPxcg5XOmUoba~6pB24UE-uz~JPBV2832a~q2UNr5Vju8yTTX4Ua3XcZDtzwSuvhhT9HCx5-rSWteaCIrZq2LibQAK5QUPSjTFw-0URhxs73k6nzJ6NGU5juzruOztIWsULtIUBRqHlIMXmUn1jF2FOdP0OOe8TxOladtdY9cQ4ZxEcWcu9a1de5O20l9SUIP6EZudNGhKEdfDfFPx~R0KyhAZzYsX43KXkONpusJ4XsHFMun1gnWNM3I~cMo9qY~XQla99oB1IPNzRtLKaIrPHal8Dn~FCuA-c1XplfR~I0LNR3bZ4NwjarmFnXRw6qSWq~FPxiNE9SaE8XBAAA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" alt="" class="imgicon">
+          <img src="https://s3-alpha-sig.figma.com/img/a16e/21de/05116c7e23b02b910bbbb0797e6a007c?Expires=1682899200&Signature=V3dPxcg5XOmUoba~6pB24UE-uz~JPBV2832a~q2UNr5Vju8yTTX4Ua3XcZDtzwSuvhhT9HCx5-rSWteaCIrZq2LibQAK5QUPSjTFw-0URhxs73k6nzJ6NGU5juzruOztIWsULtIUBRqHlIMXmUn1jF2FOdP0OOe8TxOladtdY9cQ4ZxEcWcu9a1de5O20l9SUIP6EZudNGhKEdfDfFPx~R0KyhAZzYsX43KXkONpusJ4XsHFMun1gnWNM3I~cMo9qY~XQla99oB1IPNzRtLKaIrPHal8Dn~FCuA-c1XplfR~I0LNR3bZ4NwjarmFnXRw6qSWq~FPxiNE9SaE8XBAAA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" alt="" class="imgicon">
+          <img src="https://s3-alpha-sig.figma.com/img/a16e/21de/05116c7e23b02b910bbbb0797e6a007c?Expires=1682899200&Signature=V3dPxcg5XOmUoba~6pB24UE-uz~JPBV2832a~q2UNr5Vju8yTTX4Ua3XcZDtzwSuvhhT9HCx5-rSWteaCIrZq2LibQAK5QUPSjTFw-0URhxs73k6nzJ6NGU5juzruOztIWsULtIUBRqHlIMXmUn1jF2FOdP0OOe8TxOladtdY9cQ4ZxEcWcu9a1de5O20l9SUIP6EZudNGhKEdfDfFPx~R0KyhAZzYsX43KXkONpusJ4XsHFMun1gnWNM3I~cMo9qY~XQla99oB1IPNzRtLKaIrPHal8Dn~FCuA-c1XplfR~I0LNR3bZ4NwjarmFnXRw6qSWq~FPxiNE9SaE8XBAAA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" alt="" class="imgicon">
+          <img src="https://s3-alpha-sig.figma.com/img/a16e/21de/05116c7e23b02b910bbbb0797e6a007c?Expires=1682899200&Signature=V3dPxcg5XOmUoba~6pB24UE-uz~JPBV2832a~q2UNr5Vju8yTTX4Ua3XcZDtzwSuvhhT9HCx5-rSWteaCIrZq2LibQAK5QUPSjTFw-0URhxs73k6nzJ6NGU5juzruOztIWsULtIUBRqHlIMXmUn1jF2FOdP0OOe8TxOladtdY9cQ4ZxEcWcu9a1de5O20l9SUIP6EZudNGhKEdfDfFPx~R0KyhAZzYsX43KXkONpusJ4XsHFMun1gnWNM3I~cMo9qY~XQla99oB1IPNzRtLKaIrPHal8Dn~FCuA-c1XplfR~I0LNR3bZ4NwjarmFnXRw6qSWq~FPxiNE9SaE8XBAAA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" alt="" class="imgicon">
         </div>
         <div class="col2">
+          <img src="https://s3-alpha-sig.figma.com/img/a16e/21de/05116c7e23b02b910bbbb0797e6a007c?Expires=1682899200&Signature=V3dPxcg5XOmUoba~6pB24UE-uz~JPBV2832a~q2UNr5Vju8yTTX4Ua3XcZDtzwSuvhhT9HCx5-rSWteaCIrZq2LibQAK5QUPSjTFw-0URhxs73k6nzJ6NGU5juzruOztIWsULtIUBRqHlIMXmUn1jF2FOdP0OOe8TxOladtdY9cQ4ZxEcWcu9a1de5O20l9SUIP6EZudNGhKEdfDfFPx~R0KyhAZzYsX43KXkONpusJ4XsHFMun1gnWNM3I~cMo9qY~XQla99oB1IPNzRtLKaIrPHal8Dn~FCuA-c1XplfR~I0LNR3bZ4NwjarmFnXRw6qSWq~FPxiNE9SaE8XBAAA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" alt="" class="imgicon">
         </div>
         <div class="col3">
+          <img src="https://s3-alpha-sig.figma.com/img/a16e/21de/05116c7e23b02b910bbbb0797e6a007c?Expires=1682899200&Signature=V3dPxcg5XOmUoba~6pB24UE-uz~JPBV2832a~q2UNr5Vju8yTTX4Ua3XcZDtzwSuvhhT9HCx5-rSWteaCIrZq2LibQAK5QUPSjTFw-0URhxs73k6nzJ6NGU5juzruOztIWsULtIUBRqHlIMXmUn1jF2FOdP0OOe8TxOladtdY9cQ4ZxEcWcu9a1de5O20l9SUIP6EZudNGhKEdfDfFPx~R0KyhAZzYsX43KXkONpusJ4XsHFMun1gnWNM3I~cMo9qY~XQla99oB1IPNzRtLKaIrPHal8Dn~FCuA-c1XplfR~I0LNR3bZ4NwjarmFnXRw6qSWq~FPxiNE9SaE8XBAAA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" alt="" class="imgicon">
         </div>
-        <div class="col4">
-        </div>
-        <div class="col5">
-        </div>
-        <div class="col6">
-        </div>
-        <div class="col7">
-        </div>
+        <div class="col4"><img src="https://s3-alpha-sig.figma.com/img/a16e/21de/05116c7e23b02b910bbbb0797e6a007c?Expires=1682899200&Signature=V3dPxcg5XOmUoba~6pB24UE-uz~JPBV2832a~q2UNr5Vju8yTTX4Ua3XcZDtzwSuvhhT9HCx5-rSWteaCIrZq2LibQAK5QUPSjTFw-0URhxs73k6nzJ6NGU5juzruOztIWsULtIUBRqHlIMXmUn1jF2FOdP0OOe8TxOladtdY9cQ4ZxEcWcu9a1de5O20l9SUIP6EZudNGhKEdfDfFPx~R0KyhAZzYsX43KXkONpusJ4XsHFMun1gnWNM3I~cMo9qY~XQla99oB1IPNzRtLKaIrPHal8Dn~FCuA-c1XplfR~I0LNR3bZ4NwjarmFnXRw6qSWq~FPxiNE9SaE8XBAAA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" alt="" class="imgicon"></div>
+        <div class="col5"><img src="https://s3-alpha-sig.figma.com/img/a16e/21de/05116c7e23b02b910bbbb0797e6a007c?Expires=1682899200&Signature=V3dPxcg5XOmUoba~6pB24UE-uz~JPBV2832a~q2UNr5Vju8yTTX4Ua3XcZDtzwSuvhhT9HCx5-rSWteaCIrZq2LibQAK5QUPSjTFw-0URhxs73k6nzJ6NGU5juzruOztIWsULtIUBRqHlIMXmUn1jF2FOdP0OOe8TxOladtdY9cQ4ZxEcWcu9a1de5O20l9SUIP6EZudNGhKEdfDfFPx~R0KyhAZzYsX43KXkONpusJ4XsHFMun1gnWNM3I~cMo9qY~XQla99oB1IPNzRtLKaIrPHal8Dn~FCuA-c1XplfR~I0LNR3bZ4NwjarmFnXRw6qSWq~FPxiNE9SaE8XBAAA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" alt="" class="imgicon"></div>
+        <div class="col6"><img src="https://s3-alpha-sig.figma.com/img/a16e/21de/05116c7e23b02b910bbbb0797e6a007c?Expires=1682899200&Signature=V3dPxcg5XOmUoba~6pB24UE-uz~JPBV2832a~q2UNr5Vju8yTTX4Ua3XcZDtzwSuvhhT9HCx5-rSWteaCIrZq2LibQAK5QUPSjTFw-0URhxs73k6nzJ6NGU5juzruOztIWsULtIUBRqHlIMXmUn1jF2FOdP0OOe8TxOladtdY9cQ4ZxEcWcu9a1de5O20l9SUIP6EZudNGhKEdfDfFPx~R0KyhAZzYsX43KXkONpusJ4XsHFMun1gnWNM3I~cMo9qY~XQla99oB1IPNzRtLKaIrPHal8Dn~FCuA-c1XplfR~I0LNR3bZ4NwjarmFnXRw6qSWq~FPxiNE9SaE8XBAAA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" alt="" class="imgicon"></div>
+        <div class="col7"><img src="https://s3-alpha-sig.figma.com/img/a16e/21de/05116c7e23b02b910bbbb0797e6a007c?Expires=1682899200&Signature=V3dPxcg5XOmUoba~6pB24UE-uz~JPBV2832a~q2UNr5Vju8yTTX4Ua3XcZDtzwSuvhhT9HCx5-rSWteaCIrZq2LibQAK5QUPSjTFw-0URhxs73k6nzJ6NGU5juzruOztIWsULtIUBRqHlIMXmUn1jF2FOdP0OOe8TxOladtdY9cQ4ZxEcWcu9a1de5O20l9SUIP6EZudNGhKEdfDfFPx~R0KyhAZzYsX43KXkONpusJ4XsHFMun1gnWNM3I~cMo9qY~XQla99oB1IPNzRtLKaIrPHal8Dn~FCuA-c1XplfR~I0LNR3bZ4NwjarmFnXRw6qSWq~FPxiNE9SaE8XBAAA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" alt="" class="imgicon"></div>
       </div>
     </div>
   </div>
@@ -49,67 +51,68 @@ export default {
       list: [
         {
           img_src: "https://www.themoviedb.org/t/p/original/6hbd2zP73s9pm8ndy1xwEv7Ehem.jpg",
-          weekdate:'1',
+          weekdate: "1",
         },
         {
           img_src: "https://www.themoviedb.org/t/p/original/6hbd2zP73s9pm8ndy1xwEv7Ehem.jpg",
-          weekdate:'1',
+          weekdate: "1",
         },
         {
           img_src: "https://www.themoviedb.org/t/p/original/6hbd2zP73s9pm8ndy1xwEv7Ehem.jpg",
-          weekdate:'1',
+          weekdate: "1",
         },
         {
           img_src: "https://www.themoviedb.org/t/p/original/6hbd2zP73s9pm8ndy1xwEv7Ehem.jpg",
-          weekdate:'1',
+          weekdate: "1",
         },
       ],
+      imgUrl: "https://s3-alpha-sig.figma.com/img/a16e/21de/05116c7e23b02b910bbbb0797e6a007c?Expires=1682899200&Signature=V3dPxcg5XOmUoba~6pB24UE-uz~JPBV2832a~q2UNr5Vju8yTTX4Ua3XcZDtzwSuvhhT9HCx5-rSWteaCIrZq2LibQAK5QUPSjTFw-0URhxs73k6nzJ6NGU5juzruOztIWsULtIUBRqHlIMXmUn1jF2FOdP0OOe8TxOladtdY9cQ4ZxEcWcu9a1de5O20l9SUIP6EZudNGhKEdfDfFPx~R0KyhAZzYsX43KXkONpusJ4XsHFMun1gnWNM3I~cMo9qY~XQla99oB1IPNzRtLKaIrPHal8Dn~FCuA-c1XplfR~I0LNR3bZ4NwjarmFnXRw6qSWq~FPxiNE9SaE8XBAAA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
     };
   },
 };
 </script>
 <style>
-  /* Move all styles to the style section */
-  .calendar {
-    /* Some styles here */
-    margin: 37px 0 0 120px;
-    padding: 0px;
-    width: 700px;
-    height: 400px;
-    border-radius: 15px;
-    cursor: pointer;
-    background-color: #4D226D;
-  }
-  .top-bar {
-    height: 55px;
-    display: flex;
-    justify-content: space-between;
-  }
-  .Calendar_logo {
+/* Move all styles to the style section */
+.calendar {
+  /* Some styles here */
+  margin: 37px 0 0 120px;
+  padding: 0px;
+  width: 700px;
+  height: 400px;
+  border-radius: 15px;
+  cursor: pointer;
+  background-color: #4d226d;
+}
+.top-bar {
+  height: 55px;
+  display: flex;
+  justify-content: space-between;
+}
+.Calendar_logo {
   /* logo样式 */
   display: flex;
   align-items: flex-start;
-  margin-left: 25px; 
-  padding-top:15px;
-  }
-  .Calendar_logo p {
+  margin-left: 25px;
+  padding-top: 15px;
+}
+.Calendar_logo p {
   /* logo文字样式 */
   font-size: 22px;
   margin: 0px 0px 0px 15px;
-  font-family: 'Inter';
+  font-family: "Inter";
   color: white;
-  }
-  .Search_All{
-    width: 90px;
-    height: 30px;
-    margin: 13px 16px 0px 0px;
-    background: #8557A7;
-    border-radius: 15px;
-  }
+}
+.Search_All {
+  width: 90px;
+  height: 30px;
+  margin: 13px 16px 0px 0px;
+  background: #8557a7;
+  border-radius: 15px;
+}
 
-  .Search_All p {
+.Search_All p {
   /*查看全部文字样式*/
-  font-family: 'Inter';
+  font-family: "Inter";
   font-style: normal;
   font-weight: 400;
   font-size: 13.67px;
@@ -117,44 +120,54 @@ export default {
   margin: 0px;
   padding-top: 7px;
   color: white;
-  }
-  .calendar-body{
-    position: relative;
-    height: 345px;
-    background-color: #8557A7;
-    border-radius: 0px 0px 15px 15px;
-  }
-  .weekdays {
-    display: flex;
-    justify-content: space-between;
-    margin: 0 46px 0 46px;
-    font-size: 20px;
-    color: white;
-  }
-  .weekdays p {
-    font-family: 'Alibaba PuHuiTi';
-    font-style: normal;
-    font-weight: 900;
-    font-size: 41px;
-    line-height: 56px;
-    margin: 0;
-    padding-top: 11px;
-    padding-bottom:10px;
-  }
-  .week-image {
-    position: absolute;
-    top: 100px;
-    left: 0;
-    right: 0;
-    margin: auto;
-    width: 42px;
-    height: 42px;
-    border-radius:100px;
-  }
-  .content{
-    display: flex;
-    justify-content: space-between;
-    margin: 0 43px 0 43px;
-  }
-</style>
+}
+.calendar-body {
+  position: relative;
+  height: 345px;
+  background-color: #8557a7;
+  border-radius: 0px 0px 15px 15px;
+}
+.weekdays {
+  display: flex;
+  justify-content: space-between;
+  margin: 0 46px 0 46px;
+  font-size: 20px;
+  color: white;
+}
+.weekdays p {
+  font-family: "Alibaba PuHuiTi";
+  font-style: normal;
+  font-weight: 900;
+  font-size: 41px;
+  line-height: 56px;
+  margin: 0;
+  padding-top: 11px;
+  padding-bottom: 10px;
+}
+.week-image {
+  position: absolute;
+  top: 100px;
+  left: 0;
+  right: 0;
+  margin: auto;
+  width: 42px;
+  height: 42px;
+  border-radius: 100px;
+}
+.content {
+  display: flex;
+  justify-content: space-between;
+  margin: 0 43px 0 43px;
+}
 
+.imgicon {
+  width: 42px;
+  height: 42px;
+  border-radius: 50%;
+  display: block;
+}
+
+.content {
+  display: flex;
+}
+</style>
