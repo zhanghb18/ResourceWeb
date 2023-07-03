fbe0992d21457443b02d2ecf33aad6d1fe93bdf4
Loyage <792058350@qq.com>
2023-07-03 18:08:10 +0800

    ✨ feat(UserCenter、DramaCard): 创建DramaCard，实现番剧卡片

---
+++ vue-fronted/src/components/DramaCard.vue
@@ -0,0 +1,46 @@
+<template>
+  <div style="display: inline-block;">
+    <el-card :body-style="{ padding: '0px' }" class="card">
+      <img
+        class="image"
+        :src="msg.imgSrc"
+      />
+      <div class="drama_name">{{ msg.name }}</div>
+      <div class="drama_data">{{ msg.browseNum }}浏览 {{ msg.collectNum }}收藏</div>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DramaCard",
+  props: ['msg'],
+  data() {
+    return {
+      image:
+        "https://www.themoviedb.org/t/p/original/mvolqXssikgLeUomc59cB2RkH1k.jpg",
+    };
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.card {
+  width: 216px;
+  height: 289px;
+  margin: 0 38px 28px 0;
+}
+
+.image {
+  width: 215px;
+  height: 215px;
+}
+
+.drama_name {
+  text-align: left;
+}
+
+.drama_data {
+  text-align: left;
+}
+</style>
--- vue-fronted/src/view/userpage/UserCenter.vue
+++ vue-fronted/src/view/userpage/UserCenter.vue
@@ -2,16 +2,20 @@
   <div>
     <user-header></user-header>
     <!-- 页面模板 -->
-    <el-row style="background-color: #FCF2FF">
+    <el-row style="background-color: #fcf2ff">
       <el-col :span="12" :offset="6" class="container">
         <div class="container-header">
-          <img src="https://placekitten.com/800/600" alt="background-image">
+          <img src="https://placekitten.com/800/600" alt="background-image" />
         </div>
         <el-row>
           <el-col :span="22" :offset="1" class="container-body">
             <el-row class="container-body-info">
               <el-col :span="6">
-                <img class="container-avatar" src="https://placekitten.com/200/200" alt="avatar">
+                <img
+                  class="container-avatar"
+                  src="https://placekitten.com/200/200"
+                  alt="avatar"
+                />
               </el-col>
               <el-col :span="13">
                 <div class="profile">
@@ -31,13 +35,22 @@
                 </div>
               </el-col>
             </el-row>
-            <el-row class="container-body-profile">
-              <el-col :span="16">
-                <div style="background-color: #ccc; height: 400px;"></div>
-              </el-col>
-
-              <el-col :span="5" :offset="3">
-                <div style="background-color: #ccc; height: 400px;"></div>
+            <!-- 分割线 -->
+            <el-divider></el-divider>
+            <!-- 番剧展示区域 -->
+            <el-row>
+              <el-col :span="22" class="list_area">
+                <div class="menu">
+                  <button :class="{ menu_btn: true, active_menu_btn: true }">
+                    我的片单
+                  </button>
+                  <button :class="{ menu_btn: true, active_menu_btn: false }">
+                    我的追番
+                  </button>
+                </div>
+                <div class="card_area">
+                  <DramaCard v-for="sheet in userSheetList" :msg="sheet" />
+                </div>
               </el-col>
             </el-row>
           </el-col>
@@ -48,37 +61,63 @@
 </template>
 
 <script>
-import UserHeader from '../../components/UserHeader.vue';
+import UserHeader from "../../components/UserHeader.vue";
+import DramaCard from "../../components/DramaCard.vue";
 
 export default {
   name: "ProfilePage",
   components: {
     UserHeader,
+    DramaCard,
   },
   data() {
     return {
-      userInfo:{
+      userInfo: {
         userNickName: "张后斌", // 用户名
         userSignature: "我是懒坑小子张后斌", // 个人简介
-      }
+      },
+      userSheetList: [
+        {
+          name: "番剧1",
+          imgSrc:
+            "https://www.themoviedb.org/t/p/original/mvolqXssikgLeUomc59cB2RkH1k.jpg",
+          browseNum: 100,
+          collectNum: 100,
+        },
+        {
+          name: "番ailflef剧1",
+          imgSrc:
+            "https://www.themoviedb.org/t/p/original/mvolqXssikgLeUomc59cB2RkH1k.jpg",
+          browseNum: 132,
+          collectNum: 1124,
+        },
+        {
+          name: "番剧dawl1",
+          imgSrc:
+            "https://www.themoviedb.org/t/p/original/mvolqXssikgLeUomc59cB2RkH1k.jpg",
+          browseNum: 34,
+          collectNum: 12,
+        },
+      ],
     };
   },
   methods: {
     goToUserInfo() {
-      this.$router.push('/userinfo');
-    }
+      this.$router.push("/userinfo");
+    },
   },
   created() {
     var user_token = sessionStorage.getItem("token");
     var Data = {
-      token:user_token
+      token: user_token,
     };
     var that = this;
-    this.$api.user.getUserInfo(Data)
+    this.$api.user
+      .getUserInfo(Data)
       .then(function (response) {
         if (response.data.msg === "success") {
           var statusCode = response.data.data.statusCode;
-          if(statusCode == 1) {
+          if (statusCode == 1) {
             alertBox("获取用户信息失败，错误码：1", "error", that);
           } else {
             that.userInfo.userNickName = response.data.data.nickname;
@@ -89,9 +128,9 @@ export default {
         }
       })
       .catch(function (error) {
-          alertBox("连接异常，请检查网络或稍后再试。", "error", that);
+        alertBox("连接异常，请检查网络或稍后再试。", "error", that);
       });
-  }
+  },
 };
 </script>
 
@@ -132,6 +171,7 @@ export default {
 
 .container-body-info {
   height: 100px;
+  margin-bottom: 50px;
 }
 
 .profile {
@@ -142,7 +182,7 @@ export default {
 
 .settings {
   margin-top: 30px;
-  background: #662D91;
+  background: #662d91;
   text-align: center;
   color: #fff;
   border-radius: 10px;
@@ -168,7 +208,7 @@ export default {
 .info h2 {
   margin: 0;
   text-align: left;
-  font-family: 'Source Han Sans CN';
+  font-family: "Source Han Sans CN";
   font-style: normal;
   font-weight: 400;
   font-size: 30px;
@@ -177,7 +217,7 @@ export default {
 
 .info p {
   margin: 8px 0 0;
-  font-family: 'Inter';
+  font-family: "Inter";
   font-style: normal;
   font-weight: 400;
   font-size: 20px;
@@ -189,5 +229,33 @@ export default {
   /* background-color: #662D91; */
   margin-top: 50px;
   /* max-height: 100px; */
+}
+
+.list_area {
+  margin: 0 auto;
+}
+
+.menu {
+  display: flex;
+}
+
+.menu_btn {
+  background-color: #fff;
+  border: none;
+  height: 36px;
+  font-size: 20px;
+  padding: 0 20px;
+}
+
+.menu_btn:hover {
+  background-color: #f5f5f5;
+}
+
+.active_menu_btn {
+  border-bottom: 2px solid #662d91;
+}
+
+.card_area {
+  margin-top: 20px;
 }
 </style>
