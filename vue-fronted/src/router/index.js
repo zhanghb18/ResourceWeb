85eae981cd9c55ed69340c29ace834cfcdf0a1de
荷塘雨课堂 <57313218+jizh18@users.noreply.github.com>
2023-07-30 20:04:45 +0800

    合并冲突

--- vue-fronted/src/router/index.js
+++ vue-fronted/src/router/index.js
@@ -4,7 +4,6 @@ import UserCenter from "../view/userpage/UserCenter.vue"
 import UserInfo from "../view/userpage/UserInfo.vue"
 import BanguDetail from "../view/bangu/BanguDetail.vue"
 
-<<<<<<< HEAD
 const routes = [{
   path: "/",
   name: "home",
@@ -25,25 +24,6 @@ const routes = [{
   name: "bangudetail",
   component: BanguDetail
 }]
-=======
-const routes = [
-  {
-    path: "/",
-    name: "home",
-    component: Home
-  },
-  {
-    path: "/usercenter",
-    name: "usercenter",
-    component: UserCenter
-  },
-  {
-    path: "/userinfo",
-    name: "userinfo",
-    component: UserInfo
-  }
-]
->>>>>>> develop
 
 const router = createRouter({
   history: createWebHashHistory(),
