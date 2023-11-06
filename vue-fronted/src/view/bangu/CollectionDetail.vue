ffabe0905bde98d0295007c34773a61b94bb1bb5
Loyage <792058350@qq.com>
2023-11-06 14:26:22 +0800

    ✨ feat(CollectionDetail): 收藏夹详情页面开发

--- vue-fronted/src/view/bangu/CollectionDetail.vue
+++ vue-fronted/src/view/bangu/CollectionDetail.vue
@@ -1,9 +1,27 @@
 <template>
+  <!-- 收藏夹详情页面 -->
   <div>
     <el-row class="user-center-root">
       <user-header></user-header>
       <el-col :lg="{ span: 16, offset: 4 }" class="container">
-        
+        <el-row>
+          <el-col :span="8">
+            <DramaCard :msg="collectionMsg" class="collection-card"></DramaCard>
+          </el-col>
+          <el-col :span="16" class="sheet-col">
+            <el-row :msg=sheet v-for="(sheet, index) in collectionSheets" class="sheet-row">
+              <span>
+                {{ index + 1 }}.
+              </span>
+              <div>
+                <img :src="sheet.imgSrc" alt="" class="cover-img">
+              </div>
+              <span>
+                {{ sheet.title }}
+              </span>
+            </el-row>
+          </el-col>
+        </el-row>
       </el-col>
     </el-row>
   </div>
@@ -21,8 +39,17 @@ export default {
   },
   data() {
     return {
-      collectionSheet: [
-
+      collectionMsg: {
+        title: "收藏夹1",
+        imgSrc: "http://123.56.45.70/images/1.jpg",
+        description: "这是收藏夹1的描述",
+        dramaNum: 10,
+        collectNum: 100,
+      },
+      collectionSheets: [
+        {title : "剧名1", imgSrc : "http://123.56.45.70/images/1.jpg"},
+        {title : "剧名2", imgSrc : "http://123.56.45.70/images/1.jpg"},
+        {title : "剧名3", imgSrc : "http://123.56.45.70/images/1.jpg"},
       ],
     };
   },
@@ -48,5 +75,32 @@ export default {
   height: auto;
   min-height: 90vh;
   min-width: 900px;
+}
+
+.collection-card {
+  margin-top: 15%;
+  margin-left: 15%;
+}
+
+.sheet-row {
+  height: 100px;
+  border-bottom: 1px solid #e9e9f3;
+  display: flex;
+  align-items: center;
+  margin-top: 30px;
+  font-size: 20px;
+}
+
+.sheet-col {
+  margin-top: 1%;
+  padding-left: 5%;
+}
+
+.cover-img {
+  width: 150px;
+  height: 80px;
+  margin-left: 20px;
+  margin-right: 20px;
+  object-fit: cover;
 }
 </style>
